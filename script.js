368a174da4e1441e274bace5bd3156cc6b941703
Gabriel <55036283+gabrielbl4nk@users.noreply.github.com>
2025-06-27 20:27:13 +0000

    Fix slight color selection bug and add two feature buttons

--- script.js
+++ script.js
@@ -2,23 +2,14 @@ const main = document.querySelector(".main");
 const body = document.querySelector("body");
 const colors = document.querySelector(".colors");
 const black = document.querySelector(".black");
+const gridButton = document.querySelector(".button");
+const resetButton = document.querySelector(".resetbutton");
 
 let isMouseDown = false;
 let selected = null;
 
-let sizeOfCanvas = parseInt(
-  prompt("Enter the size of each of the canvas' sides")
-);
-
-if (sizeOfCanvas > 100) {
-  sizeOfCanvas = 100;
-}
-
-if (sizeOfCanvas <= 0 || !sizeOfCanvas) {
-  sizeOfCanvas = 32;
-}
-
 const userSize = (size) => {
+  main.innerHTML = "";
   for (let i = 0; i < size * size; i++) {
     const div = document.createElement("div");
     div.style.backgroundColor = "black";
@@ -29,7 +20,7 @@ const userSize = (size) => {
   }
 };
 
-userSize(sizeOfCanvas);
+userSize(16);
 
 main.style.cursor = "crosshair";
 
@@ -44,10 +35,10 @@ const selectColor = (elem) => {
 selectColor(black);
 
 const paintSquare = (elem) => {
+  elem.style.backgroundColor = selected.getAttribute("class");
   if (parseFloat(elem.style.opacity) >= 1) {
     return;
   }
-  elem.style.backgroundColor = selected.getAttribute("class");
   elem.style.opacity = `${parseFloat(elem.style.opacity) + 0.1}`;
 };
 
@@ -71,4 +62,25 @@ main.addEventListener("mouseover", (event) => {
   if (isMouseDown && event.target !== main) {
     paintSquare(event.target);
   }
+});
+
+gridButton.addEventListener("click", () => {
+  const sizeOfCanvas = parseInt(
+    prompt("Enter the size of each of the canvas' sides")
+  );
+  if (!sizeOfCanvas) {
+    return;
+  }
+  if (sizeOfCanvas > 100) {
+    sizeOfCanvas = 100;
+  }
+  if (sizeOfCanvas <= 0) {
+    sizeOfCanvas = 32;
+  }
+  userSize(sizeOfCanvas);
+});
+
+resetButton.addEventListener("click", () => {
+  const squares = document.querySelectorAll(".main div");
+  squares.forEach((square) => (square.style.opacity = "0"));
 });
